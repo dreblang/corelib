e73b835a444a1eeefe1d2d8dd9f08ad1fbf4eb4a
Sattvik Chakravarthy <sattvik@gmail.com>
2021-01-02 23:57:07 +0530

    Adding add and del methods to header.

--- http/objects/response_header.go
+++ http/objects/response_header.go
@@ -28,10 +28,22 @@ func (obj *ResponseHeader) String() string { return "ResponseHeader" }
 
 func (obj *ResponseHeader) GetMember(name string) object.Object {
 	switch name {
+	case "add":
+		return &object.MemberFn{
+			Obj: obj,
+			Fn:  responseHeaderAdd,
+		}
+
 	case "set":
 		return &object.MemberFn{
 			Obj: obj,
 			Fn:  responseHeaderSet,
+		}
+
+	case "del":
+		return &object.MemberFn{
+			Obj: obj,
+			Fn:  responseHeaderDel,
 		}
 	}
 	return object.NewError("No member named [%s]", name)
@@ -45,6 +57,19 @@ func (obj *ResponseHeader) Equals(other object.Object) bool {
 	return false
 }
 
+func responseHeaderAdd(this object.Object, args ...object.Object) object.Object {
+	rh := this.(*ResponseHeader)
+	if len(args) == 2 {
+		if key, ok := args[0].(*object.String); ok {
+			if value, ok := args[1].(*object.String); ok {
+				rh.header.Add(key.Value, value.Value)
+				return object.NullObject
+			}
+		}
+	}
+	return object.NewError("Could not execute header add operation. Invalid arguments!")
+}
+
 func responseHeaderSet(this object.Object, args ...object.Object) object.Object {
 	rh := this.(*ResponseHeader)
 	if len(args) == 2 {
@@ -53,6 +78,17 @@ func responseHeaderSet(this object.Object, args ...object.Object) object.Object 
 				rh.header.Set(key.Value, value.Value)
 				return object.NullObject
 			}
+		}
+	}
+	return object.NewError("Could not execute header set operation. Invalid arguments!")
+}
+
+func responseHeaderDel(this object.Object, args ...object.Object) object.Object {
+	rh := this.(*ResponseHeader)
+	if len(args) == 1 {
+		if key, ok := args[0].(*object.String); ok {
+			rh.header.Del(key.Value)
+			return object.NullObject
 		}
 	}
 	return object.NewError("Could not execute header set operation. Invalid arguments!")
